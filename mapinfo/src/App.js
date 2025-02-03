1ab2e0d0e8e8679d20f11db1ff6c853325d82983
Sofia Azam <srazam5@gmail.com>
2025-02-03 18:26:12 -0500

    created react project for map component

---
+++ mapinfo/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MapComponent from "./MapComponent";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Map of...</h1>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default App;
