e2fc37fc55b0d3e4eb8c0cb967a2f6e9346f5055
Sofia Azam <srazam5@gmail.com>
2025-02-04 21:58:45 -0500

    button added to each saved location

--- mapandmore/src/pages/saved.js
+++ mapandmore/src/pages/saved.js
@@ -6,13 +6,19 @@ const Saved = () => {
         "Cary": ["NC", 28, "3:42pm"],
         "Greenville": ["NC", 34, "3:42pm"]
     };
+
+    const unsaveLocation = (key) => {
+        
+    };
+
     return (
         <div>
             <h1>Saved Locations</h1>
             <div>
                 {Object.entries(locations).map(([location, values]) => (
-                    <div class = "savedLoc">
+                    <div className = "savedLoc">
                         <h3>{location}</h3>
+                        <button type="button">Unsave Location</button>
                         <div>
                             {values.map((value, index) => (
                                 <div>{value}</div>
