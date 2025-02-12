62c50d4d04f8b87297de83b42524668ea8ad9308
Sofia Azam <srazam5@gmail.com>
2025-02-12 00:32:13 -0500

    Search suggestions appear on front end
    All search suggestions can now be shown on the application for city
    searches. Buttons will be present next to each
    line to direct each search suggestion to the respective web page about
    its weather. The buttons have not been programmed at this time.
    
    Co-author: srazam

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,12 +1,16 @@
 // dependencies
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config(); 
 const {searchCity, searchZip, getWeatherData} = require('./services/geoSearch');
 
 const app = express();
 const PORT = 8000;
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+
+app.use(cors());
+app.use(express.json());
 
 // get city recommendations based on search query (city name OR city-name,state-code,country-code)
 // URL: http://localhost:3000/citySearch?q=QUERY (replace QUERY with search query)
